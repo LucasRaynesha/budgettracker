fd3776427c9338d1481ca086be1b717650fbf8ed
RaeRay7 <105617601+RaeRay7@users.noreply.github.com>
2023-03-01 14:29:52 -0500

    classes

---
+++ src/main/java/org/perscholas/budgettracker/BudgetTrackerApplication.java
@@ -0,0 +1,16 @@
+package org.perscholas.budgettracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class BudgetTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetTrackerApplication.class, args);
+    }
+
+}
